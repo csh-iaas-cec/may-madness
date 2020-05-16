006e91ae210dcbf0ef18028501c8b36b39976b58
csh-iaas-cec <51996734+csh-iaas-cec@users.noreply.github.com>
2020-05-17 03:47:59 +0530

    added notification functionality

--- func.py
+++ func.py
@@ -5,21 +5,37 @@ import requests
 from fdk import response
 
 
-def handler(ctx, data: io.BytesIO=None):
-    name = "World"
-    headers = {
-		'Content-Type': 'application/json',
-		'Authorization': 'key=' + serverToken,
-    }
+def handler(ctx, data: io.BytesIO = None):
+    # name = "World"
     try:
         body = json.loads(data.getvalue())
-        name = body.get("name")
+        deviceToken = body.get("name")
+        serverToken = "AAAAOSxh1Ys:APA91bE1Q4hdRXG7KKXvMmdiMSb1aM5CbJS2Fw3RG9-yTrCjs8ta7JbCg_Gf55YzJkkFrABg53NezTNko0CXaqW2oVvOJEyF2DE7zUoCwm2KubTp6mFJjHRbgoPNxG4pvNruFEn_y0iC"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "key=" + serverToken,
+        }
+        to_body = {
+            "notification": {
+                "title": "Corona alert",
+                "body": "Hi you have possibility of contacting someone who has corona",
+            },
+            "to": deviceToken,
+            "priority": "high",
+            #   'data': dataPayLoad,
+        }
+        response = requests.post(
+            "https://fcm.googleapis.com/fcm/send",
+            headers=headers,
+            data=json.dumps(body),
+        )
+
     except (Exception, ValueError) as ex:
-        logging.getLogger().info('error parsing json payload: ' + str(ex))
+        logging.getLogger().info("error parsing json payload: " + str(ex))
 
     logging.getLogger().info("Inside Python Hello World function")
     return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
-        headers={"Content-Type": "application/json"}
+        ctx,
+        response_data=json.dumps({"message": "Successful"}),
+        headers={"Content-Type": "application/json"},
     )
